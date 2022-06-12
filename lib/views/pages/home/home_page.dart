f4d411c338f9b6710b5b5971aaa393456df0e88a
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2022-06-12 12:15:43 -0700

    analytic and crushlytic implemented.

--- lib/views/pages/home/home_page.dart
+++ lib/views/pages/home/home_page.dart
@@ -53,7 +53,6 @@ class _HomePageState extends State<HomePage> {
                   delegate: SearchWidget(),
                 );
               }),
-          /*
           IconButton(
             onPressed: () async {
               AwesomeDialog(
@@ -71,7 +70,6 @@ class _HomePageState extends State<HomePage> {
             icon: Icon(CupertinoIcons.cloud_upload),
             color: AppColors.white,
           ),
-          */
         ],
       ),
       body: Container(
